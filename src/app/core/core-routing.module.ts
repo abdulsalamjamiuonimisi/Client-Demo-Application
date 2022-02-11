39a3a15d463ae9e463129acead51d0f61018cd3f
jamiuonimisi <jamiuonimisi@comsoftltd.com>
2022-02-11 10:06:19 +0100

    stash

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -6,7 +6,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 const routes: Routes = [
   {
     path: '', component: LandingPageComponent,
-    canActivate: [MaslGuard]
+    // canActivate: [MaslGuard]
   },
  
 ];
